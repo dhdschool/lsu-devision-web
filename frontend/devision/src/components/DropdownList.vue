18899882164bf6605a24504084f0ea89816d07bd
Calvin Gavin <cgavin7@lsu.edu>
2025-06-17 10:27:02 -0500

    Update DropdownList to accept props

--- frontend/devision/src/components/DropdownList.vue
+++ frontend/devision/src/components/DropdownList.vue
@@ -4,16 +4,20 @@ import type {Ref} from 'vue';
 import {BDropdown, BDropdownItem, BImg} from 'bootstrap-vue-next';
 //import type {imageListItems} from './ImageSelect.vue';
 
+defineProps<{
+  items: string[];
+}>();
 
-const texts : Ref<string[]> = ref([]);
-const text : Ref<string> = ref("Test");
+// Previous trash
+// const texts : Ref<string[]> = ref([]);
+// const text : Ref<string> = ref("Test");
 </script>
 
 
 <template>
   <div>
-    <b-dropdown id="dropdown-1" text="text" class="m-md-2">
-      <b-dropdown-item v-for="(idx) in texts" :key="idx">
+    <b-dropdown id="dropdown-1" text="Select an option" class="m-md-2">
+      <b-dropdown-item v-for="(item, idx) in items" :key="idx">
         {{ text }}
       </b-dropdown-item>
     </b-dropdown>
