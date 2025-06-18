951520c7e6d811f4e8c8dfb25439c52eed5b18fb
Calvin Gavin <cgavin7@lsu.edu>
2025-06-18 17:08:53 -0500

    Creates basic pages, and creates routes for each page.

--- frontend/devision/src/router/index.ts
+++ frontend/devision/src/router/index.ts
@@ -17,6 +17,52 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    
+    {
+      path: '/account',
+      name: 'account',
+      component: () => import('../views/AccountPage.vue'),
+    },
+    {
+      path: '/base',
+      name: 'base',
+      component: () => import('../views/BasePage.vue'),
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('../views/ContactPage.vue'),
+    },
+    {
+      path: '/createAccount',
+      name: 'createAccount',
+      component: () => import('../views/CreateAccountPage.vue'),
+    },
+    {
+      path: '/help',
+      name: 'help',
+      component: () => import('../views/HelpPage.vue'),
+    },
+    {
+      path: '/home',
+      name: 'homePage',
+      component: () => import('../views/HomePage.vue'),
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginPage.vue'), 
+    },
+    {
+      path: '/predictions',
+      name: 'predictions',
+      component: () => import('../views/PredictionPage.vue'),
+    },
+    {
+      path: '/settings',
+      name: 'settings',
+      component: () => import('../views/SettingsPage.vue'), 
+    }
   ],
 })
 
