65ef58af1bcc96133d64e6d20e97a252633489a8
Calvin Gavin <cgavin7@lsu.edu>
2025-06-19 17:03:49 -0500

    Adds router links to toolbar

--- frontend/devision/src/components/Toolbar.vue
+++ frontend/devision/src/components/Toolbar.vue
@@ -7,15 +7,15 @@ console.log("Toolbar Loaded!")
 <template>
   <main>
     <div>
-        <b-button-toolbar key-nav aria-label="Toolbar with button groups">
-            <b-button-group class="mx-1">
-                <b-button>Home</b-button>
-                <b-button>Predictions</b-button>
-                <b-button>Settings</b-button>
-                <b-button>Help</b-button>
-                <b-button>Contact</b-button>
-                <b-button>Login</b-button>
-                <b-button>Account</b-button>
+        <b-button-toolbar key-nav aria-label="Toolbar with button groups" :justify="true">
+            <b-button-group class="mx-1 w-100 flex-fill">
+                <b-button as="router-link" to="/">Home</b-button>
+                <b-button as="router-link" to="/predictions">Predictions</b-button>
+                <b-button as="router-link" to="/settings">Settings</b-button>
+                <b-button as="router-link" to="/help">Help</b-button>
+                <b-button as="router-link" to="/contact">Contact</b-button>
+                <b-button as="router-link" to="/login">Login</b-button>
+                <b-button as="router-link" to="/account">Account</b-button>
             </b-button-group>
         </b-button-toolbar>
     </div>
