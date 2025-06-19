df1fbadbe4ab471ad126a576eee125164cb41209
Calvin Gavin <cgavin7@lsu.edu>
2025-06-19 14:51:35 -0500

    Creates base component for Toolbar

---
+++ frontend/devision/src/components/Toolbar.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">    
+import { BButtonToolbar } from 'bootstrap-vue-next';
+
+console.log("Toolbar Loaded!")
+</script>
+
+<template>
+  <main>
+  <h1>"Login page"</h1> 
+  <p>Under Construction</p>
+  </main>
+</template>
