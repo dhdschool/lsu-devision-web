d91864e76dfc757e0d4dfc10651b52a1e6ff89e7
Calvin Gavin <cgavin7@lsu.edu>
2025-06-20 13:02:22 -0500

    Filled some contact info for GUI team, needs gowri contacts and any missed Math dept contacts.

--- frontend/devision/src/views/ContactPage.vue
+++ frontend/devision/src/views/ContactPage.vue
@@ -4,7 +4,26 @@ console.log("Hello World")
 
 <template>
   <main>
-  <h1>"Contact Page"</h1>
-  <p>Under Construction</p>
+    <div>
+      <Toolbar/>
+      <h1>"Contact Information"</h1>
+      <!--TODO: Find and post contacts: Email, names, websites?-->
+      <p>Math Department Contacts</p>
+        <p>Peter Wolenski</p>   
+        <a href="wolenski@math.lsu.edu"></a>
+      
+      <p>GUI team contacts</p>
+        <a href="#"></a>
+        <p>Calvin Gavin</p>
+        <a href="cgavin7@lsu.edu"></a>
+        <p>Alex Mensen-Johnson</p>
+        <a href="Alex.Mensen-Johnson@lsu.edu"></a>
+        <p>Dow Draper</p>
+        <a href="ddrape6@lsu.edu"></a>
+      
+      <p>Gowri contacts</p>
+        <a href="#"></a>
+      
+    </div>
   </main>
 </template>
