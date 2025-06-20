cd7e661b8aa1c8012976a27fb0559ef92726b67a
Calvin Gavin <cgavin7@lsu.edu>
2025-06-20 17:50:56 -0500

    Adds email addresses and adds mailto property

--- frontend/devision/src/views/ContactPage.vue
+++ frontend/devision/src/views/ContactPage.vue
@@ -5,21 +5,22 @@ console.log("Hello World")
 <template>
   <main>
     <div>
-      <Toolbar/>
       <h1>"Contact Information"</h1>
       <!--TODO: Find and post contacts: Email, names, websites?-->
       <p>Math Department Contacts</p>
         <p>Peter Wolenski</p>   
-        <a href="wolenski@math.lsu.edu"></a>
+        <a href="mailto:wolenski@math.lsu.edu">Email</a>
+        <p>Nadejda Drenska</p>
+        <a href="mailto:ndrenska@lsu.edu">Email</a>
       
       <p>GUI team contacts</p>
         <a href="#"></a>
         <p>Calvin Gavin</p>
-        <a href="cgavin7@lsu.edu"></a>
+        <a href="mailto:cgavin7@lsu.edu">Email</a>
         <p>Alex Mensen-Johnson</p>
-        <a href="Alex.Mensen-Johnson@lsu.edu"></a>
+        <a href="mailto:Alex.Mensen-Johnson@lsu.edu">Email</a>
         <p>Dow Draper</p>
-        <a href="ddrape6@lsu.edu"></a>
+        <a href="mailto:ddrape6@lsu.edu">Email</a>
       
       <p>Gowri contacts</p>
         <a href="#"></a>
