9efb989998b8560143bfabcde8107f741f287138
Calvin Gavin <cgavin7@lsu.edu>
2025-06-20 13:50:14 -0500

    Base page is put as base of the App page. Toolbar is now displayed on all pages.

--- frontend/devision/src/App.vue
+++ frontend/devision/src/App.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import BasePage from './views/BasePage.vue';
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <!--<img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />-->
 
     <div class="wrapper">
      <!--<HelloWorld msg="You did it!" />-->
-
+      <BasePage/>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
