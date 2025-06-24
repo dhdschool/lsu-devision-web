31e1cd6f58e90d5a6620d8c2a1c7eb416fed02c0
alexmj003 <idkyflymf@gmail.com>
2025-06-24 10:38:43 -0500

    Fixed multiple displays in App.vue works perfectly now

--- frontend/devision/src/App.vue
+++ frontend/devision/src/App.vue
@@ -6,21 +6,10 @@ import BasePage from './views/BasePage.vue';
 <template>
   <header>
     <!--<img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />-->
-    <RouterView/>
+
     <div>
       <BasePage/>
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/account">Account</RouterLink>
-        <RouterLink to="/base">Base</RouterLink>
-        <RouterLink to="/contact">Contact</RouterLink>
-        <RouterLink to="/createAccount">Create Account</RouterLink>
-        <RouterLink to="/help">Help</RouterLink>
-        <RouterLink to="/home">Home Page</RouterLink>
-        <RouterLink to="/login">Login</RouterLink>
-        <RouterLink to="/predictions">Predictions</RouterLink>
-        <RouterLink to="/settings">Settings</RouterLink>
       </nav>
     </div>
   </header>
@@ -28,7 +17,7 @@ import BasePage from './views/BasePage.vue';
 
   </body>
 
-  <RouterView />
+  <!--<RouterView />-->
 </template>
 
 <style type="text/css">
