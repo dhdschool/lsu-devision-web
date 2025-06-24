a3f40dc103539229dc2871230f36eab02b91fd8e
Alexmj003 <idkyflymf@gmail.com>
2025-06-24 18:44:15 -0500

    fixed errors I made during pull request, re uploading Calvin's changes to the contact Page

--- frontend/devision/src/views/ContactPage.vue
+++ frontend/devision/src/views/ContactPage.vue
@@ -1,29 +1,55 @@
-<script setup lang="ts">    
+<script setup lang="ts">
 console.log("Hello World")
+import { BCard } from 'bootstrap-vue-next';
 </script>
 
 <template>
   <main>
     <div>
-      <Toolbar/>
-      <h1>"Contact Information"</h1>
-      <!--TODO: Find and post contacts: Email, names, websites?-->
-      <p>Math Department Contacts</p>
-        <p>Peter Wolenski</p>   
-        <a href="wolenski@math.lsu.edu"></a>
-      
-      <p>GUI team contacts</p>
-        <a href="#"></a>
-        <p>Calvin Gavin</p>
-        <a href="cgavin7@lsu.edu"></a>
-        <p>Alex Mensen-Johnson</p>
-        <a href="Alex.Mensen-Johnson@lsu.edu"></a>
-        <p>Dow Draper</p>
-        <a href="ddrape6@lsu.edu"></a>
-      
-      <p>Gowri contacts</p>
-        <a href="#"></a>
-      
+      <h1 class="text-center">Contact Information</h1>
+
+      <div>
+        <b-card
+          border-variant="dark"
+          text-variant="dark"
+          bg-variant="light"
+          title="Math Department Contacts"
+          class="text-center"
+        >
+          <p>Peter Wolenski<br>
+          <a href="mailto:wolenski@math.lsu.edu">wolenski@math.lsu.edu</a>
+          </p>
+        <p>Nadejda Drenska<br>
+        <a href="mailto:ndrenska@lsu.edu">ndrenska@lsu.edu</a>
+        </p>
+        </b-card>
+      </div>
     </div>
+    <div>
+      <b-card
+          border-variant="dark"
+          text-variant="dark"
+          bg-variant="light"
+          title="GUI team contacts"
+          class="text-center"
+      >
+        <div >
+          <p>Calvin Gavin<br>
+          <a href="mailto:cgavin7@lsu.edu">cgavin7@lsu.edu</a>
+          </p>
+          <p>Alex Mensen-Johnson<br>
+          <a href="mailto:Alex.Mensen-Johnson@lsu.edu">Alex.Mensen-Johnson@lsu.edu</a>
+          </p>
+          <p>Dow Draper<br>
+          <a href="mailto:ddrape6@lsu.edu">ddrape6@lsu.edu</a>
+          </p>
+          <p>Gowri Sunka<br>
+          <a href="gsunka1@lsu.edu">gsunka1@lsu.edu</a>
+          </p>
+        </div>
+      </b-card>
+      </div>
   </main>
 </template>
+
+<style type="css"></style>
