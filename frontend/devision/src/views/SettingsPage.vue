fcf7afb8dadb15f66b091724e2b3f1698103e2dc
Alexmj003 <idkyflymf@gmail.com>
2025-06-24 17:03:56 -0500

    changed appearance setting to only allow one toggle at a time

--- frontend/devision/src/views/SettingsPage.vue
+++ frontend/devision/src/views/SettingsPage.vue
@@ -1,30 +1,30 @@
 <script setup lang="ts">
-import App from "@/App.vue";
+import { ref } from 'vue'
+
 type Toggle={
   id: number;
   name: string;
   isActive: boolean;
 }
-const runtTimeToggle: Toggle[] = [
+const runtTimeToggle = ref<Toggle[]>([
   { id: 1, name: 'Automatically export to excel when predicting', isActive: true },
   { id: 2, name: 'Place Holder', isActive: false },
-]
-
-const modelFolderToggle: Toggle[] = [
+])
+const modelFolderToggle = ref<Toggle[]>([
   { id: 1, name: 'Save Model', isActive: true },
   { id: 2, name: 'Save Folder', isActive: false },
-]
+])
 
-const AppearanceToggle: Toggle[] = [
+const AppearanceToggle = ref<Toggle[]> ([
   { id: 1, name: 'Light Theme', isActive: true },
   { id: 2, name: 'Dark Theme', isActive: false },
-]
+])
 
 
 const settingsSections = [
-  { id: 1, title: 'Run Time Settings', toggles: runtTimeToggle },
-  { id: 2, title: 'Model/Folder Default Settings', toggles: modelFolderToggle },
-  { id: 3, title: 'Appearance', toggles: AppearanceToggle }
+  { id: 1, title: 'Run Time Settings', toggles: runtTimeToggle, toggleMethod: toggleSetting },
+  { id: 2, title: 'Model/Folder Default Settings', toggles: modelFolderToggle, toggleMethod: toggleSetting },
+  { id: 3, title: 'Appearance', toggles: AppearanceToggle, toggleMethod: themeToggle },
 ]
 function toggleSetting(toggles: Toggle[], id: number): Toggle[] {
   return toggles.map(toggle =>
@@ -32,8 +32,7 @@ function toggleSetting(toggles: Toggle[], id: number): Toggle[] {
   );
 }
 function themeToggle(toggles: Toggle[], id: number): Toggle[] {
-  return toggles.map(toggle =>
-    toggle.id === id ? { ...toggle, isActive: !toggle.isActive } : toggle
+  return toggles.map(toggle => ({...toggle, isActive: toggle.id === id ? !toggle.isActive : false})
   );
 }
 </script>
@@ -42,9 +41,14 @@ function themeToggle(toggles: Toggle[], id: number): Toggle[] {
       <div class="settings-container">
         <div class="settings-section" v-for="section in settingsSections" :key="section.id">
           <h2>{{ section.title }}</h2>
-          <div v-for="toggle in section.toggles" :key="toggle.id">
+          <div v-for="toggle in section.toggles.value" :key="toggle.id">
             <label>
-              <input type="checkbox" :checked="toggle.isActive" @change="section.toggles = toggleSetting(section.toggles, toggle.id)">
+              <input
+                type="checkbox"
+                :name="'section-' + section.id"
+                :checked="toggle.isActive"
+                @change="section.toggles.value = section.toggleMethod(section.toggles.value, toggle.id)"
+              >
               {{ toggle.name }}
             </label>
           </div>
