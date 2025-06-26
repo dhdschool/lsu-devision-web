79d8b102cdcb4398e7727f9a72edd7f1da3e877c
alexmj003 <idkyflymf@gmail.com>
2025-06-26 13:50:04 -0500

    created the markdown for the main page

---
+++ frontend/devision/public/home-page.md
@@ -0,0 +1,20 @@
+<p>
+This software uses Stardist Machine
+<br>
+Learning models to count both oyster
+<br>
+seeds and frog eggs. You can also
+<br>
+load in your own Stardist models for
+<br>
+Predictions
+<br>
+<br>
+To get started, click the predictions
+<br>
+tab in the toolbar to open the predicting
+<br>
+software or select the Help tab to learn
+<br>
+more about the software.
+</p>
--- frontend/devision/src/views/HomePage.vue
+++ frontend/devision/src/views/HomePage.vue
@@ -1,10 +1,30 @@
-<script setup lang="ts">    
-console.log("Hello World")
+<script setup lang="ts">
+import MarkdownIt from "markdown-it";
 </script>
 
+const MarkdownText = "# Hello World"!;
+const renderedHtml = MarkdownIt().render(MarkdownText);
 <template>
   <main>
-  <h1>"Home page"</h1> 
+  <h1>Math Clinic Counting Software</h1>
+    <h3>Welcome to our counting software!</h3>
   <p>Under Construction</p>
   </main>
 </template>
+
+<style scoped>
+h1{
+  text-align: center;
+  color : #100000;
+  padding-bottom: 1rem;
+}
+h3{
+  text-align: center;
+  color : #100000;
+  padding-bottom: 1rem;
+}
+p{
+  text-align: left;
+  color : #100000;
+}
+</style>
