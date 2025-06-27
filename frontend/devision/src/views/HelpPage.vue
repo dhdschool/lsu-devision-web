065da16238a436ca833ec442772f0dc06e2fddeb
Calvin Gavin <cgavin7@lsu.edu>
2025-06-27 10:23:41 -0500

    Creates base template Help Page

--- frontend/devision/src/views/HelpPage.vue
+++ frontend/devision/src/views/HelpPage.vue
@@ -1,10 +1,40 @@
 <script setup lang="ts">    
 console.log("Hello World")
+import { BCard } from 'bootstrap-vue-next';
 </script>
 
 <template>
   <main>
-  <h1>"Help page"</h1> 
-  <p>Under Construction</p>
+
+  <b-card 
+    border-variant="dark" 
+    text-variant="dark" 
+    bg-variant="light"
+    title="Predictions"
+    class="text-center">
+
+    <p>Text about Predictions functionality</p>
+  </b-card> 
+
+  <b-card 
+    border-variant="dark" 
+    text-variant="dark" 
+    bg-variant="light"
+    title="Settings"
+    class="text-center">
+    
+    <p>Text about functionality</p>
+  </b-card>  
+
+  <b-card 
+    border-variant="dark" 
+    text-variant="dark" 
+    bg-variant="light"
+    title="Account"
+    class="text-center">
+    
+    <p>Text about functionality</p>
+  </b-card> 
+
   </main>
 </template>
