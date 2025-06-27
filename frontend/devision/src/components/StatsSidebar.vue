20632196775d89ad2cca6e76f2327cc0f9f19ab0
Calvin Gavin <cgavin7@lsu.edu>
2025-06-27 14:55:16 -0500

    Creates basic logic for the page, makes sidebar components

---
+++ frontend/devision/src/components/StatsSidebar.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+import { BListGroup, BFormInput } from 'bootstrap-vue-next';
+
+const sizeClass = ref<string>("")
+const seedTrayWeight = ref<number>(0);
+const slideWeight = ref<number>(0);
+const combinedWeight = computed(() => seedTrayWeight.value + slideWeight.value);
+
+</script>
+
+<template>
+    <b-list-group>
+        <b-list-group-item>Stats Sidebar</b-list-group-item>
+        <b-list-group-item>Size Class<BFormInput input type ="string" v-model="sizeClass"></BFormInput></b-list-group-item>
+        <b-list-group-item>Seed Tray Weight<BFormInput input type="number" v-model.number="seedTrayWeight"></BFormInput></b-list-group-item>
+        <b-list-group-item>Slide Weight<BFormInput input type ="number" v-model.number="slideWeight"></BFormInput></b-list-group-item>
+        <b-list-group-item>Slide + Tray Weight: {{ combinedWeight }}</b-list-group-item>
+    </b-list-group>
+</template>
